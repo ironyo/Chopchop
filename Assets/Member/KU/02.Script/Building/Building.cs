78a7287cbddbd6ae1fc87eaa967cedb6a21d12b2
ironyo <kyou09kang@gmail.com>
2025-10-25 23:31:20 +0900

    BuildOffset00

--- Assets/Member/KU/02.Script/Building/Building.cs
+++ Assets/Member/KU/02.Script/Building/Building.cs
@@ -2,5 +2,62 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
+    private BoxCollider2D boxCollider;
     public BuildingSO buildingSO;
+    
+    private int level = 1;
+    private int minionCount = 0;
+
+    public int NowLevel
+    {
+        get
+        {
+            return level;
+        }
+        set
+        {
+            if (buildingSO.maxLevel+1 > value)
+                level = value;
+        }
+    }
+    public int NowMinion
+    {
+        get
+        {
+            return minionCount;
+        }
+        set
+        {
+            if(buildingSO.maxMinion+1 > value)
+                minionCount = value;
+        }
+    }
+
+    private void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+        int wSize = Mathf.RoundToInt(buildingSO.width / buildingSO.maxW);
+        boxCollider.size = new Vector2(buildingSO.maxW + 4,  wSize+ 4);
+    }
+    private void OnDrawGizmos()
+    {
+        if (boxCollider != null)
+        {
+            Gizmos.color = Color.green;
+
+            Vector3 boxPos = transform.position + (Vector3)boxCollider.offset;
+
+            Vector3 boxSize = new Vector3(boxCollider.size.x, boxCollider.size.y, 0f);
+
+            Gizmos.DrawWireCube(boxPos, boxSize);
+        }
+    }
+    public void BuildUpgrade()
+    {
+        NowLevel++;
+    }
+    public void MinionPlus(int plus)
+    {
+        NowMinion += plus;
+    }
 }
--- Assets/Member/KU/04.BuildingSO/BuildingSO.cs
+++ Assets/Member/KU/04.BuildingSO/BuildingSO.cs
@@ -5,16 +5,17 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "BuildingSO", menuName = "SO/BuildingSO")]
 public class BuildingSO : ScriptableObject
 {
-    [Header("레벨")]
-    public int level = 1;
+    [Header("최대레벨")]
+    public int maxLevel = 3;
     [Header("건물이름")]
     public string buildName;
     [Header("체력")]
     public int Health = 300;
-    [Header("건물스탯")]
+    [Header("건물크기")]
     public int width;
     public int maxW;
     [Header("건물인원수")]
-    public int minionCount;
     public int maxMinion;
+    [field:Header("레벨마다 증가")]
+    public List<int> countStet;
 }
