7ff280e5390aeb04a3e5ca2fe0aa5db2385ada4c
Bimtaeur30 <Bimtaeur30@gmail.com>
2025-11-09 23:39:42 +0900

    uiAdjust_and_MinionCounterSliderPos

--- Assets/Member/KTJ/01.Script/ResourcePref.cs
+++ Assets/Member/KTJ/01.Script/ResourcePref.cs
@@ -7,12 +7,14 @@ public class ResourcePref : MonoBehaviour
     [SerializeField] private TextMeshProUGUI countTxt;
     [SerializeField] private TextMeshProUGUI countTxt_s;
     [SerializeField] private Image icon;
+    [SerializeField] private Image icon_s;
 
     public void Set(int count, Sprite icon)
     {
         countTxt.text = count.ToString() + " :";
         countTxt_s.text = count.ToString() + " :";
         this.icon.sprite = icon;
+        this.icon_s.sprite = icon;
     }
 
     public void UpdateCount(int count)
