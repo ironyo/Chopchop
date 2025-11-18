133b477599ade4818944033da14d61e16959f46a
jjoung0911 <chjw091118@gmail.com>
2025-11-19 08:59:58 +0900

    feat : SettingUI

---
+++ Assets/Member/CHJ/02.Scripts/Ui/SettingUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using DG.Tweening;
+public class SettingUI : MonoBehaviour
+{
+    private Sequence _sequence;
+    [SerializeField] private GameObject block;
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        block.SetActive(true);
+        _sequence?.Kill();
+        _sequence = DOTween.Sequence();
+        _sequence.SetUpdate(true);
+        _sequence.Append(transform.DOScale(1, 0.5f).SetEase(Ease.InOutElastic));
+        _sequence.onComplete += () => Time.timeScale = 0;
+        _sequence.Play();
+    }
+    public void Close()
+    {
+        Time.timeScale = 1;
+        _sequence?.Kill();
+        _sequence = DOTween.Sequence();
+        _sequence.SetUpdate(true);
+        _sequence.Append(transform.DOScale(0, 0.5f).SetEase(Ease.InOutElastic));
+        _sequence.onComplete += () =>
+        {
+            block.SetActive(false);
+            gameObject.SetActive(false);
+        };
+        _sequence.Play();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
