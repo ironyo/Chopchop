5ec77597f7e0c58929c36de0b13676d2a4bb7b1c
ironyo <kyou09kang@gmail.com>
2025-11-19 17:50:51 +0900

    ryt5sewiko89gcbfxdfzrs

--- Assets/Member/KU/04.Data/BuildingSO/BuildingSO.cs
+++ Assets/Member/KU/04.Data/BuildingSO/BuildingSO.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "BuildingSO", menuName = "SO/BuildingSO")]
 public class BuildingSO : ScriptableObject
 {
+    [Header("건물이미지")]
+    public Sprite sprite;
     [Header("최대레벨")]
     public int maxLevel = 3;
     [Header("건물이름")]
