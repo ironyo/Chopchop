16fa0b4092796f8f5f817ab79c054d175145369a
ironyo <kyou09kang@gmail.com>
2025-11-20 23:09:07 +0900

    7u6556e4etsrzfsdcdvbnm

--- Assets/Member/KTJ/01.Script/InventoryUI.cs
+++ Assets/Member/KTJ/01.Script/InventoryUI.cs
@@ -6,7 +6,7 @@ public class InventoryUI : UIBase
 {
     private void Start()
     {
-        StartCoroutine(CloseEffect());
+        //StartCoroutine(CloseEffect());
     }
 
     public override IEnumerator CloseEffect()
