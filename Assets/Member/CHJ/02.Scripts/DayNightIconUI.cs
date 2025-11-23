a8006408a94e02cbe43ade18a74aad26462c9d40
jjoung0911 <chjw091118@gmail.com>
2025-11-23 19:48:36 +0900

    feat : None

--- Assets/Member/CHJ/02.Scripts/DayNightIconUI.cs
+++ Assets/Member/CHJ/02.Scripts/DayNightIconUI.cs
@@ -11,22 +11,27 @@ public class DayNightIconUI : MonoSingleton<DayNightIconUI>
         base.Awake();
     }
 
-    public void Check(string time)
+    public void Check(int time)
     {
-        if (time == "AM")
+        bool currentT = time is > 19 and < 23;
+        if (currentT)
             ChangeToDay();
         else
-            ChageToNight();
+            ChangeToNight();
     }
 
-    private void ChageToNight()
+    private void ChangeToNight()
     {
+        nightimage.DOKill();
+        dayimage.DOKill();
         dayimage.DOFade(0, 0.3f);
         nightimage.DOFade(1, 0.3f);
     }
 
     private void ChangeToDay()
     {
+        nightimage.DOKill();
+        dayimage.DOKill();
         nightimage.DOFade(0, 0.3f);
         dayimage.DOFade(1, 0.3f);
     }
